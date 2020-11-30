084160b5f4425db30f0c57acd7d81f3779636d34
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-11-30 22:45:25 +0000

    binding events

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -17,11 +17,11 @@
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="hero.firstName"/>
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName"/>
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
@@ -66,17 +66,17 @@
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button class="link card-footer-item cancel-button" @click="cancelHero()">
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item" @click="saveHero">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info">{{message}}</div>
+        <div class="notification is-info"><pre>{{message}}</pre></div>
       </div>
     </div>
   </div>
@@ -98,6 +98,15 @@ export default {
       },
       message:''
     };
+  },
+  methods: {
+    cancelHero () {
+      this.message = ''
+    },
+    saveHero(){
+      this.message = JSON.stringify(this.hero, null, '\n');
+    },
+
   }
 };
 
