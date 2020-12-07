c97bdbca09dfb92164f0f784c78957a4eb4c7ac2
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-12-07 22:41:24 +0000

    routing

---
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PageNotFound from './views/page-not-found.vue'
+// import Heroes from './views/heroes.vue'
+// import HeroDetail from './views/hero-detail.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: '/heroes'
+    },
+     {
+      path: '/heroes',
+      name: 'heroes',
+      // component: Heroes
+      component: () => import(/* webpackChunkName: "heroes" */ './views/heroes.vue'),
+    },
+    {
+      path: '/heroes/:id',
+      name: 'hero-detail',
+      // component: HeroDetail,
+      component: () => import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+      props: r => ({id: parseInt(r.params.id)}),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/about.vue')
+    },
+    {
+      path: "*",
+      component: PageNotFound,
+
+    }
+  ]
+})
